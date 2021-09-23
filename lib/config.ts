2e883f0a1c3eed087a08076706fd61daa25aa4a4
kenticohero <maartenv@kentico.com>
2021-09-23 22:18:18 +0200

    API keys updated

--- lib/config.ts
+++ lib/config.ts
@@ -3,8 +3,8 @@ import { Homepage } from '../models/homepage';
 
 export const deliveryClient = new DeliveryClient({
   // Tip: Use your own sample project ID instead of the Sample Project ID
-  projectId: "7fe16acf-6a3e-0106-78c6-677b67f73847",
-  previewApiKey: 'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAianRpIjogImJjZjk1MzllYjQxNjQ5M2Q5MzUyMzY0MDU4Yzk2OGMyIiwNCiAgImlhdCI6ICIxNjI0NDM3OTgyIiwNCiAgImV4cCI6ICIxOTcwMDM3OTgyIiwNCiAgInByb2plY3RfaWQiOiAiN2ZlMTZhY2Y2YTNlMDEwNjc4YzY2NzdiNjdmNzM4NDciLA0KICAidmVyIjogIjEuMC4wIiwNCiAgImF1ZCI6ICJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSINCn0.twhtdWDF5l4Rm5AH9b9hce-HqasHO5zLyrWCQMYSOg4',
+  projectId: "3b4e3f40-edce-017e-8b28-93376249bc93",
+  previewApiKey: 'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAianRpIjogIjgwZDgwMmMwMmMwMDRmNjRiZDQ5MTFjYzJkMTIyMTY0IiwNCiAgImlhdCI6ICIxNjMyNDI4MTM0IiwNCiAgImV4cCI6ICIxOTc4MDI4MTM0IiwNCiAgInByb2plY3RfaWQiOiAiM2I0ZTNmNDBlZGNlMDE3ZThiMjg5MzM3NjI0OWJjOTMiLA0KICAidmVyIjogIjEuMC4wIiwNCiAgImF1ZCI6ICJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSINCn0.qQoAlZYKulFfPHe4bID4M0qsrOb23EeACruGhHhz43U',
   globalQueryConfig: {
     usePreviewMode: true, // Queries the Delivery Preview API.
   },
